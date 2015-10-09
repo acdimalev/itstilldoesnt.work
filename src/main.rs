76403db4fd4e7a449fc67e9672819a6421cbd10d
Jamin <jamin@bitlag.net>
2015-10-09 01:20:49 -0700

    Travis wants a directory for deployment

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 extern crate markdown;
 
+use std::fs;
 use std::fs::File;
 
 use std::io::{Read, Write};
 
 fn main() {
+    fs::create_dir_all("html").unwrap();
     let mut md = File::open("test.md").unwrap();
-    let mut html = File::create("test.html").unwrap();
+    let mut html = File::create("html/test.html").unwrap();
     let mut s = String::new();
     md.read_to_string(&mut s).unwrap();
     html.write_all(&markdown::to_html(&s).into_bytes()).unwrap();
